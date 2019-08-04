d1c5ff888f5f83404ed2eb13a3ab1c861186c691
maoguangping <991335931@qq.com>
2019-08-04 16:15:34 +0800

    整合了sleuth-zipkin，用来查询服务深度与耗时时间：https://www.cnblogs.com/jianliang-Wu/p/8945890.html

---
+++ sleuth-zipkin/src/main/java/com/mgp/SleuthZipkinApp.java
@@ -0,0 +1,23 @@
+package com.mgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @EnableZipkinServer
+ *
+ * 用于开启Zipkin Server功能
+ *
+ */
+@EnableZipkinServer
+@SpringBootApplication
+@EnableEurekaClient
+public class SleuthZipkinApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthZipkinApp.class, args);
+    }
+
+}
