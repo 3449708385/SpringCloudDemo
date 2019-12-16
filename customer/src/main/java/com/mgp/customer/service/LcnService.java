0cfe556c38a8028a86ea4552dcaa1566481b0e80
maoguangping <991335931@qq.com>
2019-12-16 15:50:50 +0800

    有熔断的LCN成功版

--- customer/src/main/java/com/mgp/customer/service/LcnService.java
+++ customer/src/main/java/com/mgp/customer/service/LcnService.java
@@ -5,7 +5,7 @@ import com.mgp.customer.service.impl.LcnServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 
 // name: 服务提供者application.yml中的spring.application.name
-// fallback: 断路器执行方法，即方法执行失败调用
-@FeignClient(name="lcn-server-test"/*, fallback = LcnServiceFallback.class*/)
+// fallback: 断路器执行方法，即方法执行失败调用, LCN使用需要注意
+@FeignClient(name="lcn-server-test", fallback = LcnServiceFallback.class)
 public interface LcnService extends LcnApi {
 }
